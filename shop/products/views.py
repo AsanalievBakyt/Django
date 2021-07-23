accabde03aa67c89140a2f8a324cd806fb632024
AsanalievBakyt <Asanali.bk@mail.ru>
2021-07-23 11:54:36 +0600

    changes in product detail to make review, create functions to delete order and update order

--- shop/products/views.py
+++ shop/products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from .models import Product, Category, Order
-from .forms import ProductForm, OrdersForm, RegisterForm
+from .models import *
+from .forms import *
 from django.http import HttpResponse
 from .services import use_promo
+from django.utils import timezone
+
 
 
 def homepage(request):
@@ -33,7 +35,11 @@ def product_post(request):
 
 def my_orders(request):
     orders = Order.objects.filter(user=request.user)
-    return render(request,'my_orders.html', context={'orders': orders})
+    now = timezone.now()
+    aval = timezone.timedelta(minutes=10)
+    return render(request,'my_orders.html', context={'orders': orders,
+                                                     'now': now,
+                                                     'aval':aval})
 
 def order_create(request,product_name):
     product = Product.objects.get(name__iexact=product_name)
@@ -73,7 +79,16 @@ def user_registr(request):
 
 def product_detail(request, product_id):
     product = Product.objects.get(id=product_id)
-    return render(request, 'product_detail.html',context={'p': product})
+    o = Order.objects.filter(product=product, status='closed')
+    form = ReviewForm()
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data.get('text')
+            Review.objects.create(user=request.user, product=product, text=text)
+    return render(request, 'product_detail.html',context={'p': product,
+                                                          'form': form,
+                                                          'o': o})
 
 def product_update(request, product_id):
     product = Product.objects.get(id=product_id)
@@ -91,4 +106,27 @@ def delete_product(request, product_id):
         product.delete()
         return redirect('home')
     return render(request, 'product_delete.html', context={'p': product})
+
+def order_update(request, order_id):
+    order = Order.objects.get(id=order_id)
+    form = OrdersForm(instance=order)
+    if request.method == 'POST':
+        now = timezone.now()
+        aval = timezone.timedelta(minutes=10)
+        diff = now - order.date
+        if diff <= aval:
+            form = OrdersForm(request.POST, instance=order)
+            if form.is_valid():
+                form.save()
+                return redirect('my_orders')
+        else:
+            return HttpResponse(f'время истекло')
+    return render(request,'order_create.html', context={'form': form})
+
+def delete_order(request, order_id):
+    order = Order.objects.get(id=order_id)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('my_orders')
+    return render(request, 'delete_order.html', context={'o': order})
 
