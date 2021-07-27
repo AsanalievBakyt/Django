e60c1af6a649bb0ee303322f9e2af226c93a91f0
AsanalievBakyt <Asanali.bk@mail.ru>
2021-07-27 11:55:19 +0600

    add settings for EMAIL

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -131,3 +131,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'car.shop.bakyt@gmail.com'
+EMAIL_HOST_PASSWORD = 'testpassword123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
